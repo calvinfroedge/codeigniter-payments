76cdb8222ec59130c8c55d1421c0c2aa060efd8c
Calvin Froedge <calvinfroedge@gmail.com>
2011-07-05 15:16:04 -0500

    Updated readme and paypal paymentspro config

---
+++ README.md
@@ -0,0 +1,9 @@
+Load spark like this:
+
+$this->load->spark('cf_payments/1.0.0/');
+
+You can find examples for PayPal PaymentsPro in:
+
+/controllers/paypal_paymentspro_examples.php
+
+Make sure you add your config settings to the payment module config before attempting an API call.
--- config/payment_modules/paypal_paymentspro.php
+++ config/payment_modules/paypal_paymentspro.php
@@ -1,8 +1,8 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['paypal_api_username'] = "iambrs_1298074268_biz_api1.gmail.com";
-$config['paypal_api_password'] = "1298074286";
-$config['paypal_api_signature'] = "Awe05O9DgD-XyAL3-HsFoqNs..1VAOncRYkwEN.LCh-94svEO5c0i0Ar";
+$config['paypal_api_username'] = "";
+$config['paypal_api_password'] = "";
+$config['paypal_api_signature'] = "";
 $config['paypal_api_endpoint'] = "https://api-3t.sandbox.paypal.com/nvp?";
 $config['paypal_api_version'] = "66.0";
 
